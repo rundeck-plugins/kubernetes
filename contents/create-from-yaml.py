2fe93709552fa7465946f4b3f4cde3fc15b2166f
Pramodh KP <pramodh@rapyuta-robotics.com>
2020-11-18 19:16:45 +0530

    Added support for openshift buildconfig

--- contents/create-from-yaml.py
+++ contents/create-from-yaml.py
@@ -5,9 +5,9 @@ import os
 import yaml
 import common
 
-from kubernetes import client
+from kubernetes import client, config
 from kubernetes.client.rest import ApiException
-
+from openshift.dynamic import DynamicClient
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO,
                     format='%(levelname)s: %(name)s: %(message)s')
@@ -130,6 +130,16 @@ def main():
 
             print(common.parseJson(resp.status))
 
+        if data["type"] == "BuildConfig":
+            dep = yaml.safe_load(data["yaml"])
+
+            k8s_client = config.new_client_from_config()
+            openshift_client = DynamicClient(k8s_client)
+            v1_bc = openshift_client.resources.get(api_version='build.openshift.io/v1', kind='BuildConfig')
+
+            resp = v1_bc.create(body=dep, namespace='default')
+
+            print(common.parseJson(resp.metadata))
     except ApiException as e:
         log.error("Exception error creating: %s\n" % e)
         sys.exit(1)
